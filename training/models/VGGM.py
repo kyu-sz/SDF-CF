c50927a42760bef9e29c73d0c1e1a00503620b9f
Ken Yu <kyu_115s@hotmail.com>
2018-02-20 15:14:32 -0500

    Finish distractors.

--- training/models/VGGM.py
+++ training/models/VGGM.py
@@ -8,25 +8,6 @@ import torch.utils.model_zoo as model_zoo
 import numpy as np
 
 
-class LRN(nn.Module):
-    def __init__(self):
-        super(LRN, self).__init__()
-
-    def forward(self, x):
-        #
-        # x: N x C x H x W
-        pad = Variable(x.data.new(x.size(0), 1, 1, x.size(2), x.size(3)).zero_())
-        x_sq = (x ** 2).unsqueeze(dim=1)
-        x_tile = torch.cat((torch.cat((x_sq, pad, pad, pad, pad), 2),
-                            torch.cat((pad, x_sq, pad, pad, pad), 2),
-                            torch.cat((pad, pad, x_sq, pad, pad), 2),
-                            torch.cat((pad, pad, pad, x_sq, pad), 2),
-                            torch.cat((pad, pad, pad, pad, x_sq), 2)), 1)
-        x_sumsq = x_tile.sum(dim=1).squeeze(dim=1)[:, 2:-2, :, :]
-        x = x / ((2. + 0.0001 * x_sumsq) ** 0.75)
-        return x
-
-
 class VGGM(nn.Module):
     def __init__(self, model_path=None, model_url=None):
         super(VGGM, self).__init__()
--- training/test_discriminative.py
+++ training/test_discriminative.py
@@ -60,20 +60,10 @@ if __name__ == '__main__':
                 if lu[1] == ll[1] or lu[0] == ru[0]:
                     continue
 
-                # cv2.line(canvas, pt2tuple(p0), pt2tuple(p1), b)
-                # cv2.line(canvas, pt2tuple(p1), pt2tuple(p2), b)
-                # cv2.line(canvas, pt2tuple(p2), pt2tuple(p3), b)
-                # cv2.line(canvas, pt2tuple(p3), pt2tuple(p0), b)
-                # cv2.line(canvas, pt2tuple(ext_p0), pt2tuple(ext_p1), g)
-                # cv2.line(canvas, pt2tuple(ext_p1), pt2tuple(ext_p2), g)
-                # cv2.line(canvas, pt2tuple(ext_p2), pt2tuple(ext_p3), g)
-                # cv2.line(canvas, pt2tuple(ext_p3), pt2tuple(ext_p0), g)
-                # cv2.line(canvas, pt2tuple(lu), pt2tuple(ll), r)
-                # cv2.line(canvas, pt2tuple(ll), pt2tuple(rl), r)
-                # cv2.line(canvas, pt2tuple(rl), pt2tuple(ru), r)
-                # cv2.line(canvas, pt2tuple(ru), pt2tuple(lu), r)
-
-                target = frame[int(lu[1]):int(ll[1]), int(lu[0]):int(ru[0]), :]
+                x_min = int(lu[0])
+                x_max = int(ru[0])
+                y_min = int(lu[1])
+                y_max = int(ll[1])
             else:
                 continue
                 p0 = bbox[1:-1:-1]
@@ -83,18 +73,49 @@ if __name__ == '__main__':
                 x_max = int(max(p0[0], p1[0]))
                 y_min = int(min(p0[1], p1[1]))
                 y_max = int(max(p0[1], p1[1]))
-                target = frame[y_min:y_max, x_min:x_max, :]
-            #
-            # cv2.imshow(seq['name'] + ' origin', canvas)
-            # cv2.imshow(seq['name'] + ' target', target)
-            # cv2.waitKey(1)
 
-            input_tensor = np.zeros((1, 3, 224, 224), dtype=np.float32)
+            target = frame[y_min:y_max, x_min:x_max, :]
+
+            width = x_max - x_min
+            height = y_max - y_min
+            x_left = np.maximum(0, x_min - width / 2)
+            x_right = np.minimum(frame.shape[1], x_max + height / 2)
+            x_mid = (x_min + x_max) / 2
+            y_upper = np.maximum(0, y_min - width / 2)
+            y_lower = np.minimum(frame.shape[0], y_max + height / 2)
+            y_mid = (y_min + y_max) / 2
+            distractors = [frame[y_upper:y_mid, x_left:x_mid, :],
+                           frame[y_upper:y_mid, x_min:x_max, :],
+                           frame[y_upper:y_mid, x_mid:x_right, :],
+                           frame[y_min:y_max, x_left:x_mid, :],
+                           frame[y_min:y_max, x_min:x_max, :],
+                           frame[y_min:y_max, x_mid:x_right, :],
+                           frame[y_mid:y_lower, x_left:x_mid, :],
+                           frame[y_mid:y_lower, x_min:x_max, :],
+                           frame[y_mid:y_lower, x_mid:x_right, :],
+
+                           frame[y_upper:y_max, x_left:x_max, :],
+                           frame[y_upper:y_max, x_min:x_right, :],
+                           frame[y_min:y_lower, x_left:x_max, :],
+                           frame[y_min:y_lower, x_min:x_right, :],
+
+                           frame[y_min:y_mid, x_min:x_mid, :],
+                           frame[y_min:y_mid, x_mid:x_max, :],
+                           frame[y_mid:y_max, x_min:x_mid, :],
+                           frame[y_mid:y_max, x_mid:x_max, :]]
+
+            input_tensor = np.zeros((len(distractors) + 1, 3, 224, 224), dtype=np.float32)
             input_tensor[0, :] = np.transpose(
                 cv2.cvtColor(
                     cv2.resize(target, (224, 224)),
                     cv2.COLOR_BGR2RGB),
                 (2, 0, 1))
+            for i in range(len(distractors)):
+                input_tensor[i + 1, :] = np.transpose(
+                    cv2.cvtColor(
+                        cv2.resize(distractors[i], (224, 224)),
+                        cv2.COLOR_BGR2RGB),
+                    (2, 0, 1))
 
             feat = mdnet.forward(Variable(torch.from_numpy(input_tensor),
                                           requires_grad=False))
@@ -107,6 +128,7 @@ if __name__ == '__main__':
             if vgg16_last_feat is not None:
                 vgg16_dist.append(torch.sum(torch.pow(feat - vgg16_last_feat, 2)).data.cpu().numpy()[0])
             vgg16_last_feat = feat
+            vgg16_total_dist += sum(vgg16_dist)
 
         if len(mdnet_dist) > 0:
             mdnet_seq_dist = np.average(mdnet_dist)
